738bf33bd6464c9706f1eea0d42005d20d5fbd7d
Daniel Júnior <daniel.junior.ifpb@gmail.com>
2023-06-28 13:03:10 -0300

    feat: httt-server protocol

--- src/infra/http/express-adapter.ts
+++ src/infra/http/express-adapter.ts
@@ -1,10 +1,16 @@
-import { Request, Response } from "express";
+import express from "express";
+import HttpServer from "./http-server";
 
-export default class ExpressAdapter {
-    static create(fn: Function) {
-        return async function (req: Request, res: Response) {
-            const obj = await fn(req.params, req.body);
-            res.json(obj);
-        }
+export default class ExpressAdapter implements HttpServer {
+    private app: any
+    constructor() {
+        this.app = express();
+        this.app.use(express.json())
+    }
+    on(method: string, url: string, callback: Function): void {
+        throw new Error("Method not implemented.");
+    }
+    listen(port: number): void {
+        this.app.listen(port);
     }
 }
--- src/main.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import ExpressAdapter from './infra/http/express-adapter';
-import AlarmController from './presentation/alarm-controller';
-const app = express();
-app.use(express.json())
-app.get('alarms', ExpressAdapter.create(AlarmController.getAlarms))
-app.listen(3000)
